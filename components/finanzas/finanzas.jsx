e4f4337903fd021160b3026643ef5f2725ac6598
DanielPalacios05 <drpalaciod@eafit.edu.co>
2023-03-29 15:00:35 -0500

    finanzas

---
+++ components/finanzas/finanzas.jsx
@@ -0,0 +1,41 @@
+
+
+export default function Finanzas () {
+
+    return ( <div className="courses-container">
+    <div className="course">
+        <div className="course-image">
+            <img src="finanzas1.jpg"/>
+        </div>
+        <div className="course-info">
+            <h3 className="course-title">10 consejos para mejorar tu puntaje crediticio</h3>
+            <p className="course-description">
+            Este artículo explica la importancia del puntaje crediticio y proporciona diez consejos prácticos para mejorar tu puntaje crediticio. Incluye consejos como mantener saldos bajos en las tarjetas de crédito, pagar las facturas a tiempo y revisar tu informe crediticio regularmente.
+            </p>
+        </div>
+    </div>
+    <div className="course">
+        <div className="course-image">
+            <img src="finanzas2.jpg"/>
+        </div>
+        <div className="course-info">
+            <h3 className="course-title">Cómo crear un presupuesto personal en 5 sencillos pasos</h3>
+            <p className="course-description">
+            En este artículo se explica cómo crear un presupuesto personal para controlar tus gastos y ahorrar dinero. Se describen los cinco pasos básicos para crear un presupuesto efectivo, incluyendo el seguimiento de los gastos, la creación de categorías de gastos y la fijación de metas de ahorro.
+            </p>
+        </div>
+    </div>
+    <div className="course">
+        <div className="course-image">
+            <img src="finanzas3.jpg"/>
+        </div>
+        <div className="course-info">
+            <h3 className="course-title">Gastos innecesarios: cómo identificarlos y eliminarlos de tu presupuesto</h3>
+            <p className="course-description">
+            Este artículo explora los gastos innecesarios que pueden afectar tus finanzas personales y proporciona consejos para identificar y eliminar estos gastos de tu presupuesto. Incluye consejos prácticos como cancelar 
+            </p>
+        </div>
+    </div>
+    </div>
+    )
+}
